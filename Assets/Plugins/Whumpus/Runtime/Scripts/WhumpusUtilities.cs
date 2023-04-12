4266d74c5fa17f96742bf81a8b0e4750e20727e0
WhumpusWu <whumpus.wu@gmail.com>
2023-04-12 14:45:43 +0200

    dfsf

---
+++ Assets/Plugins/Whumpus/Runtime/Scripts/WhumpusUtilities.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Whumpus
+{
+    public class WhumpusUtilities
+    {
+        public static int ToLayer(int bitmask)
+        {
+            int result = bitmask > 0 ? 0 : 31;
+            while (bitmask > 1)
+            {
+                bitmask = bitmask >> 1;
+                result++;
+            }
+            return result;
+        }
+    }
+}
